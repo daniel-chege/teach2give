4630b822e71b09298d421870bd47ddd57801f543
daniel-chege <126775594+daniel-chege@users.noreply.github.com>
2024-05-06 13:09:00 +0300

    MY FIRST COMMIT

---
+++ capitalize.py
@@ -0,0 +1,10 @@
+'''Write a program that accepts a string as input, capitalizes the first letter of each word in the 
+string, and then returns the result string.
+Examples: 
+"hi"=> returns "Hi"
+"i love programming"=> returns "I Love Programming"'''
+a = "i love programming"
+capitalized_string = a.title()
+ 
+print("a:", a)
+print("Capitalized String:", capitalized_string)
